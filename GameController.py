d22495b4060e318d1e3cb39b67c7557e2673acd5
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-04-12 16:46:05 +0200

    move pawn

--- GameController.py
+++ GameController.py
@@ -11,6 +11,8 @@ class GameController:
         self.clock = pg.time.Clock()
         pg.font.init()
         self.font = pg.font.SysFont('Comic Sans MS', 30)
+        self.next_is_move = False
+        self.active_cell = None
 
     def update(self):
         pg.display.flip()
@@ -28,7 +30,6 @@ class GameController:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
-
             for i in range(8):
                 for j in range(8):
                     cell = self.game.board.board_array[i][j]
@@ -37,14 +38,29 @@ class GameController:
                         if event.button == 1:
                             if cell.rect.collidepoint(event.pos):
                                 print('clicked  ' + cell.piece)
-                                self.game.board.reset_cells(self.screen, self.font)
-                                possible_moves = cell.focus(self.screen, self.font)
-                                if possible_moves:
-                                    possibles_moves_cells = self.game.board.get_cells_from_locations(possible_moves)
-                                    print(possibles_moves_cells)
+                                print(self.next_is_move)
+                                # first click to select the piece
+                                if not self.next_is_move:
+                                    self.game.board.reset_cells(self.screen, self.font)
+                                    possible_moves = cell.focus(self.screen, self.font)
+                                    self.active_cell = cell 
+                                    if possible_moves:
+                                        self.next_is_move = True
+                                        possibles_moves_cells = self.game.board.get_cells_from_locations(possible_moves)
+                                        print(possibles_moves_cells)
 
-                                    for cell in possibles_moves_cells:
-                                        cell.focus_possible_move(self.screen)
+                                        for cell in possibles_moves_cells:
+                                            cell.focus_possible_move(self.screen)
+                                #second click to move the piece
+                                else:
+                                    print('move piece')
+                                    cell.piece = self.active_cell.piece
+                                    self.active_cell.piece = None
+                                    self.game.board.reset_cells(self.screen, self.font)
+                                    self.next_is_move = False
+                                    self.active_cell = None
+
+
 
     def run(self):
         self.draw()
